6d49d60feb38cea6556561fcddf199280bc3a5a2
BernedoItalo <rodriguezbernedo.italomanuel.studente@itispaleocapa.it>
2026-02-03 08:15:47 +0100

    Add files via upload

---
+++ main Memory.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/*Realizzare il gioco del Memory con le seguenti funzionalità:
+
+
+Fase preparatoria
+
+il programma genera casualmente la dimensione della matrice quadrata scegliendo un numero tra 4 e 7;
+in caso di matrice con numero di elementi dispari inserire lo zero in una posizione casuale;
+il programma inserisce nella matrice le coppie di caratteri in modo casuale;
+il programma visualizza una matrice di asterischi (tessere coperte);
+Gioco
+l'utente inserisce le coordinate di due tessere;
+il programma scopre le due tessere,  se sono uguali le lascia scoperte, se sono diverse le ricopre dopo 2 secondi;
+il gioco finisce quando vengono scoperte tutte le tessere;
+a fine gioco il programma segnala il numero di tentativi effettuati dal giocatore;
+Effettuare almeno un commit  per ogni funzionalità elencata. */
+
+int main(int argc, char** argv) {
+	return 0;
+}
